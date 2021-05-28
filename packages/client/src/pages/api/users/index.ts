675e855c3ca9067bc55ec8221582f01d6fc80731
Guy Thomas <guy.thomas@instacart.com>
2021-05-28 12:53:42 -0700

    add callback url to add users post signup

--- packages/client/src/pages/api/users/index.ts
+++ packages/client/src/pages/api/users/index.ts
@@ -1,21 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import postgres from 'postgres';
 
+import { connectionConfig } from '@the-last-resume/db'
+const sql = postgres(connectionConfig)
 
 function protectAuth0Handler(request: NextApiRequest, response: NextApiResponse){
+    if (!process.env.AUTH0_SECRET) console.warn('AUTH0_SECRET is not configured. Requests will fail')
+    
     const isAuthorizedRequest = request.body.event.secrets.AUTH0_SECRET !== process.env.AUTH0_SECRET
+    
     if (isAuthorizedRequest) response.status(401).end()
 }
 
-function createUser(request: NextApiRequest, response: NextApiResponse) {
-    console.log('Creating new user', request.body)
+async function createUser(request: NextApiRequest, response: NextApiResponse) {
+    const { id, username, email } = request.body?.event?.user || {}
+    if (!id) response.status(422).end()
+    await sql`
+        insert into app_public.users ( auth_id, username, email ) VALUES (${id}, ${username}, ${email});
+    `
     response.status(200).end()
 }
 
 export default function (request: NextApiRequest, response: NextApiResponse) {
-    if (request.method === 'POST') {
-        protectAuth0Handler(request, response)
-        createUser(request, response)
-    } else {
-        response.status(405).end()
-    }
+    return new Promise(resolve => {
+        switch (request.method) {
+          case "POST": {
+            protectAuth0Handler(request, response)
+            createUser(request, response)
+            break;
+          }
+          default: {
+            response.status(405).end()
+          }
+        }
+        return resolve(true)
+      })
 }
